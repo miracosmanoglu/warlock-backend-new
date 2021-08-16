51666d5b92a741b82ebb8e46142fd7893a105bee
miracosmanoglu <mrccc23@gmail.com>
2021-08-16 12:03:42 +0300

    feat: added cors

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 
 // app.use(express.json());
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/admin", require("./routes/admin"));
