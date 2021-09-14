7fcb0760098903c227fe9ef85b1d60eabacea3b3
miracosmanoglu <mrccc23@gmail.com>
2021-09-14 16:57:31 +0300

    .

--- index.ts
+++ index.ts
@@ -5,7 +5,6 @@ import cors from "cors";
 const app = express();
 
 // app.use(express.json());
-app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
@@ -21,6 +20,8 @@ app.use("/api/horoscopeDescription", require("./routes/horoscopeDescription"));
 app.use("/api/warlock", require("./routes/warlock"));
 app.use("/api/password", require("./routes/password"));
 app.use("/api/odeme", require("./routes/odeme"));
+
+app.use(cors());
 
 const server = app.listen(process.env.PORT || 3001, () =>
   console.log("🚀 Server ready at: http://localhost:3001")
